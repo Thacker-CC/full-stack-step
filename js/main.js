5cbe6d52620779674ae45aaf1e759c91f3a025f9
Thacker <2351531501@qq.com>
2026-08-10 17:05:51 +0800

    bugfix-import cards & score

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 import { initNav } from "./nav.js"
-import { initCardsAnim } from "./cards"
-import { initScoreAnim } from "./score"
+import { initCardsAnim } from "./cards.js"
+import { initScoreAnim } from "./score.js"
